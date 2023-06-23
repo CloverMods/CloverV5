dfbb4f376d27fd8e8c239f242c087086a5abe757
Clovermods <111981866+CloverMods@users.noreply.github.com>
2023-06-22 21:11:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -90,3 +90,24 @@ client.ev.on('creds.update', saveCreds)
 }
 
 clover_mods(), (err) => console.log("[ Error ]", color(String(err), 'red'));
+
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+
+
+var app = express()
+var mainrouter = require('./main')
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
+
+app.use('/', mainrouter);
+//app.use('/api', mainrouter);
+
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080")
+})
